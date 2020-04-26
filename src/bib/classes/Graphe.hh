53f56665294b5a004ba69ab64dd5bd6e72689b9e
gnouf1 <jeuxforum26@gmail.com>
2020-04-26 13:41:30 +0200

    ajout graphe

---
+++ src/bib/classes/Graphe.hh
@@ -0,0 +1,13 @@
+#include "Arc.hh"
+#include "Sommet.hh"
+#include "Matrice.hh"
+#include <vector>
+#include <cstring>
+
+class Graphe{
+private:
+  String etiquette;
+  vector<Arc> liste_Arcs;
+  vector<Sommet> liste_Sommets;
+  String path;
+}
