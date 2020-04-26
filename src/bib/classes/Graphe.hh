d753d1ecd36be0bca0851f3d6ed876cff4affc1c
gnouf1 <jeuxforum26@gmail.com>
2020-04-26 13:44:29 +0200

    base en cours

--- src/bib/classes/Graphe.hh
+++ src/bib/classes/Graphe.hh
@@ -10,4 +10,18 @@ private:
   vector<Arc> liste_Arcs;
   vector<Sommet> liste_Sommets;
   String path;
+
+public:
+  Graphe(String nom, vector<Sommet> listeS, vector<Arc> listeA, String path);
+  Graphe(String nom);
+  Graphe(Matrice& M);
+  Graphe(Graphe& G);
+  Graphe(vector<vector<int>> liste_voisin);
+  ~Graphe();
+
+  Matrice conversion_vers_Matrice_adj();
+  Matrice conversion_vers_Matrice_inc();
+  vector<vector<int>>  conversion_vers_listeDeVoisins();
+  int ajout_Sommet(int id, int posx, int posy);
+  
 }
