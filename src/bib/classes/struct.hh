9964f3ddcd88138fd9b167a2f0071c5fb55eb0ae
gnouf1 <jeuxforum26@gmail.com>
2020-04-26 13:18:58 +0200

    struct (sans ifdef nécessaire)

--- src/bib/classes/struct.hh
+++ src/bib/classes/struct.hh
@@ -1,7 +1,16 @@
 #include <stdlib>
+#include <cstring>
 
 typedef struct Valeur_Vecteur{
   bool type; // Si 0 entier, si 1 réel.
   int valeur_entiere;
   float valeur_reel;
-}
+}VectVal;
+
+typedef struct ROW{
+  int tache;
+  String nom_tache;
+  int duree;
+  vector <int> taches_anterieures;
+  vector <int> taches_posterieures;
+} pert_row;
