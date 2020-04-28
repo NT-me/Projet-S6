ef232b516a58ae096d59a023f5f810de7e2cbf41
gnouf1 <jeuxforum26@gmail.com>
2020-04-28 12:18:34 +0200

    bugfix

--- test/test_bib/classes/Arc.cc
+++ test/test_bib/classes/Arc.cc
@@ -5,116 +5,112 @@
 
 
 TEST_CASE ("Test des setters", "[Arc]"){
-  REQUIRE(false);
+  map<string, VectVal> m;
 
+  Arc A1 ("arc1",1, 2,3, m); //map a faire
 
-  // map<string, VectVal> m;
-  //
-  // Arc A1 ("arc1",1, 2,3, m); //map a faire
-  //
-  // SECTION ("Set ID"){
-  //   A1.setID (37);
-  //   REQUIRE (A1.getID() == 37);
-  // }
-  // SECTION ("Set etiquette"){
-  //   A1.setEtiq("modif");
-  //   REQUIRE (A1.getEtiq() == "modif");
-  // }
-  // SECTION ("Set IDdepart"){
-  //   A1.setIDDepart (7);
-  //   REQUIRE (A1.getIDDepart() == 7);
-  // }
-  // SECTION ("Set IDarrive"){
-  //   A1.setIDArrive (44);
-  //   REQUIRE (A1.getIDArrive() == 44);
-  // }
-  // SECTION ("Set ACharge_Utile"){ // A faire
-  //   A1.setCU ();
-  //   REQUIRE (A1.ACharge_Utile == );
-  // }
+  SECTION ("Set ID"){
+    A1.setID (37);
+    REQUIRE (A1.getID() == 37);
+  }
+  SECTION ("Set etiquette"){
+    A1.setEtiq("modif");
+    REQUIRE (A1.getEtiq() == "modif");
+  }
+  SECTION ("Set IDdepart"){
+    A1.setIDDepart (7);
+    REQUIRE (A1.getIDDepart() == 7);
+  }
+  SECTION ("Set IDarrive"){
+    A1.setIDArrive (44);
+    REQUIRE (A1.getIDArrive() == 44);
+  }
+  SECTION ("Set ACharge_Utile"){ // A faire
+    A1.setCU ();
+    REQUIRE (A1.getCU() == m);
+  }
+}
+
+TEST_CASE ("Test des getters", []){
+  Arc A1 ("arc1",1,2,3,m); //map a faire
+
+  SECTION ("Get ID"){
+      REQUIRE (A1.getID() == 1);
+  }
+  SECTION ("Get etiquette"){
+    REQUIRE (A1.getEtiq () == "arc1");
+  }
+  SECTION ("Get IDdepart"){
+    REQUIRE (A1.getIDDepart() == 2);
+  }
+  SECTION ("Get IDarrive"){
+    REQUIRE (A1.getIDArrive() == 3);
+  }
+  SECTION ("Get ACharge_Utile"){
+    REQUIRE (A1.getCU () == NULL) // A faire
+  }
 }
 
 
-// TEST_CASE ("Test des getters", []){
-//   Arc A1 ("arc1",1,2,3,m); //map a faire
-//
-//   SECTION ("Get ID"){
-//       REQUIRE (A1.getID() == 1);
-//   }
-//   SECTION ("Get etiquette"){
-//     REQUIRE (A1.getEtiq () == "arc1");
-//   }
-//   SECTION ("Get IDdepart"){
-//     REQUIRE (A1.getIDDepart() == 2);
-//   }
-//   SECTION ("Get IDarrive"){
-//     REQUIRE (A1.getIDArrive() == 3);
-//   }
-//   SECTION ("Get ACharge_Utile"){
-//     REQUIRE (A1.getCU () == NULL) // A faire
-//   }
-// }
-//
-//
-//
-// TEST_CASE("Test des constructeur de la classe", [arc] ){
-//
-//   SECTION ("Test du constructeur avec tout les arguments"){
-//     Arc A1 ("arc1", 1,2,3,m); // Faire la map
-//
-//     REQUIRE (A1.getEtiq() == "arc1");
-//     REQUIRE (A1.getID() == 1);
-//     REQUIRE (A1.getIDdepart () == 2);
-//     REQUIRE (A1.getIDarrive() == 3);
-//     REQUIRE (A1.getCU() == NULL); // A faire
-//   }
-//
-//   SECTION ("Test du constructeur sans la map"){
-//     Arc A2 ("arc2",2,1,4);
-//
-//     REQUIRE (A2.getEtiq() == "arc2");
-//     REQUIRE (A2.getID() == 2);
-//     REQUIRE (A2.getIDdepart() == 1);
-//     REQUIRE (A2.getIDarrive() == 4);
-//
-//       SECTION ("Test du constructeur par copie"){
-//         Arc A4 (A2);
-//
-//         REQUIRE (A4.getEtiq() == "arc2");
-//         REQUIRE (A4.getID() == 2);
-//         REQUIRE (A4.getIDdepart() == 1);
-//         REQUIRE (A4.getIDarrive() == 4);
-//       }
-//   }
-//
-//   SECTION ("Test du constructeur avec un ID, un id de sommet de départ et d'arrive"){
-//     Arc A3 (3,4,1);
-//
-//     REQUIRE (A3.getID() == 3);
-//     REQUIRE (A3.getIDdepart() == 4);
-//     REQUIRE (A3.getIDarrive() == 1);
-//   }
-// }
-//
-// TEST_CASE ("Test du destructeur de la classe", [arc]){
-//   // A faire
-// }
-//
-// TEST_CASE ("Test de l'opérateur =", [arc]){
-//   Arc A2 ("arc2",2,1,4);
-//   Arc A3 ("arc3",3,4,1);
-//   A3 = A2;
-//
-//   REQUIRE (A3.getEtiq() == "arc2");
-//   REQUIRE (A3.getID() == 2);
-//   REQUIRE (A3.getIDdepart() == 1);
-//   REQUIRE (A3.getIDarrive() == 4);
-// }
-//
-// TEST_CASE ("Test de l'opérateur ==", [arc]){
-//   Arc A2 ("arc2",2,1,4);
-//   Arc A4 (A2);
-//
-//   REQUIRE ((A2 == A4));
-//
-// }
+
+TEST_CASE("Test des constructeur de la classe", [arc] ){
+
+  SECTION ("Test du constructeur avec tout les arguments"){
+    Arc A1 ("arc1", 1,2,3,m); // Faire la map
+
+    REQUIRE (A1.getEtiq() == "arc1");
+    REQUIRE (A1.getID() == 1);
+    REQUIRE (A1.getIDdepart () == 2);
+    REQUIRE (A1.getIDarrive() == 3);
+    REQUIRE (A1.getCU() == NULL); // A faire
+  }
+
+  SECTION ("Test du constructeur sans la map"){
+    Arc A2 ("arc2",2,1,4);
+
+    REQUIRE (A2.getEtiq() == "arc2");
+    REQUIRE (A2.getID() == 2);
+    REQUIRE (A2.getIDdepart() == 1);
+    REQUIRE (A2.getIDarrive() == 4);
+
+      SECTION ("Test du constructeur par copie"){
+        Arc A4 (A2);
+
+        REQUIRE (A4.getEtiq() == "arc2");
+        REQUIRE (A4.getID() == 2);
+        REQUIRE (A4.getIDdepart() == 1);
+        REQUIRE (A4.getIDarrive() == 4);
+      }
+  }
+
+  SECTION ("Test du constructeur avec un ID, un id de sommet de départ et d'arrive"){
+    Arc A3 (3,4,1);
+
+    REQUIRE (A3.getID() == 3);
+    REQUIRE (A3.getIDdepart() == 4);
+    REQUIRE (A3.getIDarrive() == 1);
+  }
+}
+
+TEST_CASE ("Test du destructeur de la classe", [arc]){
+  // A faire
+}
+
+TEST_CASE ("Test de l'opérateur =", [arc]){
+  Arc A2 ("arc2",2,1,4);
+  Arc A3 ("arc3",3,4,1);
+  A3 = A2;
+
+  REQUIRE (A3.getEtiq() == "arc2");
+  REQUIRE (A3.getID() == 2);
+  REQUIRE (A3.getIDdepart() == 1);
+  REQUIRE (A3.getIDarrive() == 4);
+}
+
+TEST_CASE ("Test de l'opérateur ==", [arc]){
+  Arc A2 ("arc2",2,1,4);
+  Arc A4 (A2);
+
+  REQUIRE ((A2 == A4));
+
+}
