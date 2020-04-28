f8dc8ff099e4545d7f09e678ccfe1c18c657f7da
Utilisateur <user@debian.arcahe.ovh>
2020-04-28 17:38:42 +0200

    test matrice

--- test/test_bib/classes/Matrice.cc
+++ test/test_bib/classes/Matrice.cc
@@ -75,6 +75,18 @@ TEST_CASE ("Test du destructeur de la classe", "[Matrice]"){
   // A faire
 }
 
+TEST_CASE("test modification d’une case","[matrice]"){
+  Matrice MQ(3,3,4);
+  MQ.modifTab(0,1,1);
+  MQ.modifTab(1,2,1);
+  MQ.modifTab(2,0,1);
+
+  MQ.modifTab(1,2,10);
+
+  REQUIRE((MQ.getTab()[1][2] == 10));
+
+}
+
 TEST_CASE ("Test de l'opérateur =", [matrice]){
   Matrice MI1 (1,3,1);
   Matrice MI2 (2,5,1);
@@ -94,21 +106,119 @@ TEST_CASE ("Test de l'opérateur ==", "[Matrice]"){
 }
 
 TEST_CASE ("Test de conversion en matrice d'incidence", "[Matrice]"){
+  Matrice MA1(3);
+  MA1.modifTab(0,1,1);
+  MA1.modifTab(1,2,1);
+  MA1.modifTab(2,0,1);
+
+  Matrice MI1(3,3,1);
+  MI1.modifTab(0,0,1);
+  MI1.modifTab(1,1,1);
+  MI1.modifTab(2,2,1);
+  MI1.modifTab(0,2,-1);
+  MI1.modifTab(1,0,-1);
+  MI1.modifTab(2,1,-1);
+
+  MA1.conversion_incidence();
+
+  Matrice MQ(3,3,4);
+
+  REQUIRE ((MQ.conversion_incidence() == -1));
+  REQUIRE ((MA1 == MI1));
 
 }
 
 TEST_CASE ("Test d'inversion de la matrice", "[Matrice]"){
+  Matrice MA1(3);
+  MA1.modifTab(0,1,1);
+  MA1.modifTab(1,2,1);
+  MA1.modifTab(2,0,1);
+
+  Matrice MAI1(3);
+  MAI1.modifTab(0,2,1);
+  MAI1.modifTab(1,0,1);
+  MAI1.modifTab(2,1,1);
+
+  MA1.inversion_Matrice();
+
+  REQUIRE((MA1 == MAI1));
 
 }
 
-TEST_CASE ("Test de la conversion en graphe", "[Matrice]"){
+TEST_CASE ("test de sommet isolé d'un graphe", "[matrice]"){
+  Matrice MA1(3);
+  MA1.modifTab(0,1,1);
+  MA1.modifTab(1,2,1);
+  MA1.modifTab(2,0,1);
 
+  Matrice M2(3);
+  M2.modifTab(1,0,1);
+
+  REQUIRE((MA1.Sommet_non_isole() == 1 && M2.Sommet_non_isole() == 0))
+
+
+}
+
+
+TEST_CASE ("Test de la conversion en graphe", "[Matrice]"){
+  Matrice MA1(3);
+  MA1.modifTab(0,1,1);
+  MA1.modifTab(1,2,1);
+  MA1.modifTab(2,0,1);
+
+  Matrice MI1(3,3,1);
+  MI1.modifTab(0,0,1);
+  MI1.modifTab(1,1,1);
+  MI1.modifTab(2,2,1);
+  MI1.modifTab(0,2,-1);
+  MI1.modifTab(1,0,-1);
+  MI1.modifTab(2,1,-1);
+
+  Graphe G1("testG1")
+  G1.ajout_Sommet(0,0,0);
+  G1.ajout_Sommet(1,20,20);
+  G1.ajout_Sommet(2,0,30);
+  G1.ajout_Arc(0,1);
+  G1.ajout_Arc(1,2);
+  G1.ajout_Arc(2,0);
+
+  Graphe G2("g2");
+  Graphe G3("g3")
+
+  G2 = MA1.conversionGraphe();
+  G3 = MI1.conversionGraphe();
+
+  REQUIRE((G1 == G2 && G1 == G3));
 }
 
 TEST_CASE ("Test  de supression de ligne", "[Matrice]"){
+  Matrice MQ(3,3,4);
+  MQ.modifTab(0,1,1);
+  MQ.modifTab(1,2,1);
+  MQ.modifTab(2,0,1);
+
+  MQ.supprLigne(1);
+
+  Matrice MQ2(2,3,4)
+  MQ2.modifTab(0,1,1);
+  MQ2.modifTab(1,0,1);
+
+  REQUIRE((MQ == MQ2));
 
 }
 
 TEST_CASE ("Test de supression de colonne", "[Matrice]"){
+  Matrice MQ(3,3,4);
+  MQ.modifTab(0,1,1);
+  MQ.modifTab(1,2,1);
+  MQ.modifTab(2,0,1);
 
+  MQ.supprCol(1);
+
+  Matrice MQ3(3,2,4)
+
+  MQ3.modifTab(1,1,1);
+  MQ3.modifTab(2,0,1);
+
+  REQUIRE((MQ == MQ3));
 }
