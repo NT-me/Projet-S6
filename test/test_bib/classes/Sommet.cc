7f6a6152cba2da822fbcedb235c3bed53afa1a9b
gnouf1 <jeuxforum26@gmail.com>
2020-04-28 16:27:41 +0200

    test ok

--- test/test_bib/classes/Sommet.cc
+++ test/test_bib/classes/Sommet.cc
@@ -1,9 +1,14 @@
 #define CATCH_CONFIG_MAIN
 #include "../../../libExt/catch.hpp"
 #include "../../../src/bib/classes/Sommet.hh"
+#include <map>
 
 TEST_CASE ("Test des setters", "[Sommet]"){
   map<string, VectVal> m;
+  VectVal v0;
+  v0.type = false;
+  v0.valeur_entiere = 1;
+  m.insert(pair<string, VectVal> ("poid", v0));
 
   Sommet S0 (1,2,"sommet0",0,m);
 
@@ -24,106 +29,133 @@ TEST_CASE ("Test des setters", "[Sommet]"){
       REQUIRE (S0.getEtiq() == "sommet0");
     }
     SECTION ("Set SCharge_utile"){
-      S0.setCU (m);      // A faire
+      map<string, VectVal> m0;
+      VectVal v1;
+      v0.type = true;
+      v0.valeur_reel = 10.1;
+      m0.insert(pair<string, VectVal> ("poid", v1));
+
+      S0.setCU (m0);
+      REQUIRE (S0.getCU() == m0);
+    }
+}
+
+TEST_CASE ("Test des getters", "[Sommet]"){
+
+  map<string, VectVal> m;
+  VectVal v0;
+  v0.type = false;
+  v0.valeur_entiere = 12;
+
+  m.insert(pair<string, VectVal> ("plus_tot", v0));
+
+  Sommet S0 (23,32,"sommet0",0,m);
+
+    SECTION ("Get x"){
+      REQUIRE (S0.getPosX() == 23);
+    }
+    SECTION ("Get y"){
+      REQUIRE (S0.getPosY() == 32);
+    }
+    SECTION ("Get ID"){
+      REQUIRE (S0.getID() == 0);
+    }
+    SECTION ("Get etiquette"){
+      REQUIRE (S0.getEtiq () == "sommet0");
+    }
+    SECTION ("Get charge utile"){
       REQUIRE (S0.getCU () == m);
     }
 }
 
-// TEST_CASE ("Test des getters", "[Sommet]"){
-//
-//   Sommet S0 (23,32,"sommet0",0,m);
-//
-//     SECTION ("Get x"){
-//       REQUIRE (S0.getPosX() == 23);
-//     }
-//     SECTION ("Get y"){
-//       REQUIRE (S0.getPosY() == 32);
-//     }
-//     SECTION ("Get ID"){
-//       REQUIRE (S0.getID() == 0);
-//     }
-//     SECTION ("Get etiquette"){
-//       REQUIRE (S0.getEtiq () == "sommet0");
-//     }
-//
-//     SECTION ("Get map"){
-//       REQUIRE (S0.getCU () == NULL); // A faire
-//     }
-//
-// }
-//
-//
-// TEST_CASE("Test des constructeur de la classe", "[Sommet]" ){
-//
-//     SECTION ("Test du constructeur avec tout les arguments"){
-//       Sommet S1 (2,3,"sommet1",1,vec);
-//
-//       REQUIRE (S1.getPosX() == 2);
-//       REQUIRE (S1.getPosY() == 3);
-//       REQUIRE (S1.getID() == 1);
-//       REQUIRE (S1.getEtiq () == "sommet1");
-//
-//       REQUIRE (S1.getCU () == NULL); // A faire
-//     }
-//
-//     SECTION ("Test du constructeur sans map"){
-//       Sommet S2 (2,3,"sommet2",2);
-//
-//       REQUIRE (S2.getPosX() == 2);
-//       REQUIRE (S2.getPosY() == 3);
-//       REQUIRE (S2.getID() == 2);
-//       REQUIRE (S2.getEtiq () == "sommet2");
-//     }
-//
-//     SECTION ("Test du constructeur avec une etiquette et un ID"){
-//       Sommet S3 ("sommet3", 3);
-//
-//       REQUIRE (S3.getID () == 3);
-//       REQUIRE (S3.getEtiq () == "sommet3");
-//     }
-//
-//     SECTION ("Test du constructeur avec un id "){
-//       Sommet S4 (4);
-//
-//       REQUIRE (S4.getID() == 4);
-//
-//         SECTION ("Test du constructeur par copie"){
-//           Sommet S5 (S4);
-//
-//           REQUIRE (S5.getID() == 4);
-//         }
-//     }
-//
-// }
-//
-// TEST_CASE ("Test du destructeur de la classe", "[Sommet]"){
-//
-//   // A faire
-//
-// }
-//
-//
-// TEST_CASE ("Test de l'opérateur =", "[Sommet]"){
-//   Sommet S2 (2,3,"sommet2", 2);
-//   Sommet S5 (4);
-//   S5 = S2;
-//
-//   REQUIRE (S5.getPosX() == 2);
-//   REQUIRE (S5.getPosY () == 3);
-//   REQUIRE (S5.getEtiq() == "sommet2");
-//   REQUIRE (S5.getID() == 2);
-//
-// }
-//
-// /*
-// Raison pour laquelle il y a deux parenthèse dans le REQUIRE
-// https://stackoverflow.com/questions/59770581/cant-use-overloaded-comparison-operator-with-catch-test
-// */
-// TEST_CASE ("Test de l'opérateur ==", "[Sommet]"){
-//   Sommet S0 (23,32,"sommet0",0,m);
-//   Sommet S1 (2,3,"sommet1",1,vec);
-//   // Faire les map
-//
-//   REQUIRE ((S0 == S1));
-//
-// }
+TEST_CASE("Test des constructeur de la classe", "[Sommet]" ){
+
+    SECTION ("Test du constructeur avec tous les arguments"){
+      map<string, VectVal> cu;
+      VectVal v0;
+      v0.type = false;
+      v0.valeur_entiere = 12;
+
+      cu.insert(pair<string, VectVal> ("plus_tot", v0));
+
+      Sommet S0(23,32,"sommet0",0,cu);
+
+      REQUIRE (S0.getPosX() == 23);
+      REQUIRE (S0.getPosY() == 32);
+      REQUIRE (S0.getID() == 0);
+      REQUIRE (S0.getEtiq () == "sommet0");
+      REQUIRE (S0.getCU () == cu);
+    }
+
+    SECTION ("Test du constructeur sans map"){
+      Sommet S2 (2,3,"sommet2",2);
+
+      REQUIRE (S2.getPosX() == 2);
+      REQUIRE (S2.getPosY() == 3);
+      REQUIRE (S2.getID() == 2);
+      REQUIRE (S2.getEtiq () == "sommet2");
+    }
+
+    SECTION ("Test du constructeur avec une etiquette et un ID"){
+      Sommet S3 ("sommet3", 3);
+
+      REQUIRE (S3.getID () == 3);
+      REQUIRE (S3.getEtiq () == "sommet3");
+    }
+
+    SECTION ("Test du constructeur avec un id "){
+      Sommet S4 (4);
+
+      REQUIRE (S4.getID() == 4);
+
+        SECTION ("Test du constructeur par copie"){
+          Sommet S5 (S4);
+
+          REQUIRE (S5.getID() == 4);
+        }
+    }
+
+}
+
+TEST_CASE ("Test du destructeur de la classe", "[Sommet]"){
+
+}
+
+TEST_CASE ("Test de l'opérateur =", "[Sommet]"){
+  Sommet S2 (2,3,"sommet2", 2);
+  Sommet S5 (4);
+  S5 = S2;
+
+  REQUIRE (S5.getPosX() == 2);
+  REQUIRE (S5.getPosY () == 3);
+  REQUIRE (S5.getEtiq() == "sommet2");
+  REQUIRE (S5.getID() == 2);
+}
+
+/*
+Raison pour laquelle il y a deux parenthèse dans le REQUIRE
+https://stackoverflow.com/questions/59770581/cant-use-overloaded-comparison-operator-with-catch-test
+*/
+TEST_CASE ("Test de l'opérateur ==", "[Sommet]"){
+  map<string, VectVal> m;
+  VectVal v0;
+  v0.type = false;
+  v0.valeur_entiere = 1;
+  m.insert(pair<string, VectVal> ("poid", v0));
+
+  map<string, VectVal> m1;
+  VectVal v1;
+  v1.type = false;
+  v1.valeur_entiere = 100;
+  m1.insert(pair<string, VectVal> ("poid", v1));
+
+  Sommet S0 (23,32,"sommet0",0,m);
+  Sommet S0_0 (23,32,"sommet0",0,m);
+  
+  Sommet S1 (2,3,"sommet1",1,m1);
+  // Faire les map
+
+  REQUIRE ((S0 != S1));
+  REQUIRE ((S0_0 == S0));
+
+}
