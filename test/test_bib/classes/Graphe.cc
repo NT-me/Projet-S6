904e41895967af7dcd113470fccbd943afd7281a
Sebast2711 <oillosebast@gmail.com>
2020-04-28 11:23:31 +0200

    Base des test pour les graphes

--- test/test_bib/classes/Graphe.cc
+++ test/test_bib/classes/Graphe.cc
@@ -10,8 +10,6 @@ TEST_CASE ("Test des getters", []){
 
 }
 
-
-
 TEST_CASE("Test des constructeur de la classe", [graphe] ){
 
 }
@@ -27,5 +25,29 @@ TEST_CASE ("Test de l'opérateur =", [graphe]){
 
 TEST_CASE ("Test de l'opérateur ==", [graphe]){
 
+
+}
+
+TEST_CASE ("Test de conversion en matrice d'adjacence et d'incidence", [graphe]){
+
+}
+
+TEST_CASE ("Test ajout de sommet", [graphe]){
+
+}
+
+TEST_CASE ("Test suprression de sommet", [graphe]){
+
+}
+
+TEST_CASE ("Test ajout d'arc", [graphe]){
+
+}
+
+TEST_CASE ("Test de suppression d'arc", [graphe]){
+
+}
+
+TEST_CASE ("Test de conversion en liste de voisin", [graphe]){
 
 }
