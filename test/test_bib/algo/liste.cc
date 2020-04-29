0d0840fe6281095b416604c3b46fd3f1be92ddfa
gnouf1 <jeuxforum26@gmail.com>
2020-04-29 18:43:37 +0200

    début base test

---
+++ test/test_bib/algo/liste.cc
@@ -0,0 +1,98 @@
+#define CATCH_CONFIG_MAIN
+#include "../../../libExt/catch.hpp"
+#include "../../../src/bib/algo/liste.hh"
+#include <utility>
+
+#ifndef listeTest
+#define listeTest
+
+TEST_CASE("Calcul Bellman","[Algorithmes]"){
+
+}
+
+TEST_CASE("Calcul Floyd-Warshall","[Algorithmes]"){
+
+}
+
+TEST_CASE("liste floyd","[Algorithmes]"){
+
+}
+
+TEST_CASE("calcul degres entrant","[Algorithmes]"){
+
+}
+
+TEST_CASE("calcul degres sortant","[Algorithmes]"){
+
+}
+
+TEST_CASE("calcul degres entrant sortant","[Algorithmes]"){
+
+}
+
+TEST_CASE("coloration","[Algorithmes]"){
+
+}
+
+TEST_CASE("couleur adjacence","[Algorithmes]"){
+
+}
+
+TEST_CASE("stables","[Algorithmes]"){
+
+}
+
+TEST_CASE("cliques","[Algorithmes]"){
+
+}
+
+TEST_CASE("voisin sommet","[Algorithmes]"){
+
+}
+
+TEST_CASE("gestion de flots","[Algorithmes]"){
+
+}
+
+TEST_CASE("calcul posterite","[Algorithmes]"){
+
+}
+
+TEST_CASE("pert","[Algorithmes]"){
+
+}
+
+TEST_CASE("arbo","[Algorithmes]"){
+
+}
+
+TEST_CASE("anti-arbo","[Algorithmes]"){
+
+}
+
+TEST_CASE("","[Algorithmes]"){
+
+}
+
+TEST_CASE("","[Algorithmes]"){
+
+}
+
+TEST_CASE("","[Algorithmes]"){
+
+}
+
+TEST_CASE("","[Algorithmes]"){
+
+}
+
+TEST_CASE("","[Algorithmes]"){
+
+}
+
+TEST_CASE("","[Algorithmes]"){
+
+}
+
+
+#endif
