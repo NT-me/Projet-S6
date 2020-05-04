678c2e94f656476098c7ae030406dc8a6d5fb90b
salsabildf <salsabil.dafrane@ens.uvsq.fr>
2020-05-04 22:11:42 +0200

    degres + liste voisins

--- src/bib/algo/liste.cc
+++ src/bib/algo/liste.cc
@@ -6,11 +6,36 @@ pair<Matrice, Matrice> calcul_Floyd_Warshall(Matrice M){}
 
 vector<int> liste_floyd(Matrice Parent, int deb, int fin){}
 
-int calcul_degres_entrant(Matrice M, int id){}
+int calcul_degres_entrant(Matrice M, int ID){
+    
+    /* déclare variable int vide  */
+        int nb_entrant = 0;
+    
+    /* ittération sur le nombre de sommet de la matrice en paramètre */
+        for (int i=0; i < M.gettV() ; i++){
+            if (M.getTab()[i][ID]) nb_entrant++;
+         }
+    return nb_entrant;
+    }
 
-int calcul_degres_sortant(Sommet S){}
+int calcul_degres_sortant(Sommet S){
+    
+    /* déclare variable int vide  */
+        int nb_sortant = 0;
+    
+    /* ittération sur la taille du vecteur de sommet S, contenant les arcs sortants */
+        for (int i=0;  i < S.getVecArc().size();  i++){
+             nb_sortant ++;
+         }
+    return nb_sortant;
+    }
 
-pair<int, int> calcul_degres_entrant_sortant(Matrice M, Sommet S){}
+pair<int, int > calcul_degres_entrant_sortant(Matrice M, Sommet S){
+
+     pair<int,int> result( calcul_degres_entrant(M, S.getID()) , calcul_degres_sortant(S) );
+
+     return result;
+    }
 
 vector<int> coloration_Graphe(Graphe G){}
 
@@ -20,7 +45,19 @@ vector<vector<int>> stables_Graphe(Matrice M){}
 
 vector<vector<int>> cliques_Graphe(Matrice M){}
 
-vector<int> voisin_sommet(Matrice M, int ID){}
+vector<int> voisin_sommet(Matrice M, int ID){
+    
+    /* déclare variable vector vide  */
+    vector<int> voisins;
+     
+     /* ittération sur le nombre de sommet de la matrice M  */
+        for (int i=0; i < M.gettV() ; i++){
+            if ( M.getTab()[i][ID] ) {
+                voisins.insert(voisins.end(),i);
+            }
+        }
+    return voisins;
+    }
 
 int gestion_flots(Graphe G, int ID_source, int ID_puit){}
 
