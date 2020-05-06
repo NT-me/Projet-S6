be3ee6261edd2c796d459918a005d56a4ea55c72
salsabildf <salsabil.dafrane@ens.uvsq.fr>
2020-05-06 13:30:08 +0200

    degre + voisinSommet

--- src/bib/algo/liste.cc
+++ src/bib/algo/liste.cc
@@ -1,26 +1,193 @@
 #include "liste.hh"
 
-pair<vector<vector<int>>, vector<int>> calcul_Bellman(Matrice M, Sommet S){}
+pair<vector<vector<int>>, vector<int>> calcul_Bellman(Matrice M, Sommet S){
+  vector<int> dist;
+  vector<int> file;
+  vector<int> pred;
+  vector<vector<int>> liste_res;
+  pair <vector<vector<int>>,vector<int>> res;
 
-pair<Matrice, Matrice> calcul_Floyd_Warshall(Matrice M){}
+  if(M.getType()!=0){
+    std::cout << "ERROR WRONG MATRICE TYPE" << '\n';
+    return res;
+  }
+  else{
+    for(int i=0;i<M.gettV();i++){
+        dist.push_back(INFINI);
+        pred.push_back({-1});
+     }
+     dist[S.getID()] = 0;
 
-vector<int> liste_floyd(Matrice Parent, int deb, int fin){}
+    int cmpA = 0,cmpW = 0;
+    for(int i=0;i<M.gettV();i++){     // Nb Arcs
+      for(int j=0;j<M.gettV();j++){
+        if(M.getTab()[i][j])
+        cmpA++;//renvoie le nombre d'arcs
+      }
+    }
+    file.push_back(S.getID());
+    while(file[0]!=-1 && cmpW<cmpA-1){ // Bellman (poids des chemins)
+      for(int j=0;j<M.gettV();j++){
+        if(M.getTab()[file[0]][j]!=0){
+          if(dist[j] > (dist[file[0]] + M.getTab()[file[0]][j])){
+            dist[j] = dist[file[0]] + M.getTab()[file[0]][j];
+            file.push_back(j);
+            pred[j] = file[0];
+          }
+        }
+      }
+      if(file.size()==1){
+        file[0]=-1;
+      }
+      else {
+        file.erase(file.begin());
+      }
+      cmpW++;
+    }
+    // Cycles négatifs
+    for(int i=0;i<M.gettV();i++){
+      for(int j=0;j<M.gettV();j++){
+        if(M.getTab()[i][j]!=0){
+          if(dist[j] > (dist[i] + M.getTab()[i][j])){
+            std::cout << "NEGATIVE CYCLE" << '\n';
+            return res;
+          }
+        }
+      }
+    }
+    // Liste prédécesseurs
+    for(int i=0;i<pred.size();i++){
+      if(pred[i] == -1){
+        liste_res.push_back({S.getID()});
+      }
+      else if(pred[i]==S.getID()){
+        liste_res.push_back({S.getID(),i});
 
-int calcul_degres_entrant(Matrice M, int ID){
-    
-    /* déclare variable int vide  */
+
+      }
+      else{
+        int j = i;
+        vector<int> tmp;
+        vector<int> tmp2;
+        while(j!=S.getID()){
+          tmp.push_back(j);
+          j = pred[j];
+        }
+        tmp.push_back(S.getID());
+        tmp2.resize(tmp.size());
+        for(int a=0;a<tmp.size();a++){  // Remet à l'endroit
+          tmp2[a] = tmp[tmp.size()-a-1];
+        }
+        liste_res.push_back(tmp2);
+      }
+    }
+
+    res.first = liste_res;
+    res.second = dist;
+    return res;
+  }
+}
+
+//################# FLOYD Warshall  ######################
+pair<Matrice, Matrice> calcul_Floyd_Warshall(Matrice M){
+  //parcourt de la matrice d'adjacence
+  if(M.getType()!=0){
+    Matrice MP(M.gettV(),M.gettV(),2),MT(M.gettV(),M.gettV(),3);
+    pair<Matrice,Matrice> res{MP,MT};
+    std::cout << "ERROR WRONG MATRICE TYPE" << '\n';
+    return res; //retour de pair<Matrice,Matrice> vide
+  }
+  else{
+    //initialisation de MatriceParent
+    Matrice MP(M.gettV(),M.gettV(),3);
+    for(int i=0;i<M.gettV();i++){
+      for(int j=0;j<M.gettV();j++){
+        if(M.getTab()[i][j]!=0){
+          MP.modifTab(i,j,j);
+        }
+        else
+        MP.modifTab(i,j,-1);// il n'y a pas de parent direct
+
+      }
+    }
+
+    //init de Matrice de poids
+    Matrice MT(M.gettV(),M.gettV(),2);
+    for(int i=0;i<M.gettV();i++){
+      for(int j=0;j<M.gettV();j++){
+        if(i==j){
+          MT.modifTab(i,j,0);
+        }
+        else if(M.getTab()[i][j]!=0){
+          MT.modifTab(i,j,M.getTab()[i][j]);
+        }
+        else{
+          MT.modifTab(i,j,INFINI);// il n'y a pas de chemin
+
+        }
+
+      }
+    }
+    for (int k = 0; k < M.gettV();k++) {
+      for (int i = 0; i < M.gettV(); i++) {
+        for (int j = 0; j < M.gettV(); j++) {
+        //  if(k!=j&&i!=j&&k!=i){
+            if(MT.getTab()[i][j]> (MT.getTab()[i][k]+MT.getTab()[k][j])){
+              MT.modifTab(i,j,MT.getTab()[i][k]+MT.getTab()[k][j]);
+              MP.modifTab(i,j,MP.getTab()[i][k]);
+            }
+
+        //  }
+        }
+      }
+    }
+    //création de la pair<Matrice,Matrice> res
+    pair<Matrice,Matrice> res{MT,MP};
+    return res;
+
+  }
+}
+
+vector<int> liste_floyd(Matrice Parent, int deb, int fin){
+  if(Parent.getType()!=3){
+    std::cout << "ERROR WRONG TYPE" << '\n';
+    vector<int> res{-1};
+    return res;
+  }
+  else{
+    vector<int> res{deb};
+    while(Parent.getTab()[deb][fin]!=fin){
+      if(Parent.getTab()[deb][fin]!=-1){
+        deb = Parent.getTab()[deb][fin];
+        res.push_back(deb);
+
+      }
+      else{
+        std::cout << "PAS DE CHEMIN" << '\n';
+        vector<int> res{-2};
+        return res;
+      }
+    }
+    res.push_back(fin);
+    return res;
+  }
+}
+
+int calcul_degres_entrant(Matrice M, int id){
+
+     /* déclare variable int vide  */
         int nb_entrant = 0;
     
     /* ittération sur le nombre de sommet de la matrice en paramètre */
         for (int i=0; i < M.gettV() ; i++){
-            if (M.getTab()[i][ID]) nb_entrant++;
+            if (M.getTab()[i][id]) nb_entrant++;
          }
     return nb_entrant;
-    }
+}
 
 int calcul_degres_sortant(Sommet S){
-    
-    /* déclare variable int vide  */
+        
+      /* déclare variable int vide  */
         int nb_sortant = 0;
     
     /* ittération sur la taille du vecteur de sommet S, contenant les arcs sortants */
@@ -28,14 +195,13 @@ int calcul_degres_sortant(Sommet S){
              nb_sortant ++;
          }
     return nb_sortant;
-    }
+}
 
-pair<int, int > calcul_degres_entrant_sortant(Matrice M, Sommet S){
-
-     pair<int,int> result( calcul_degres_entrant(M, S.getID()) , calcul_degres_sortant(S) );
+pair<int, int> calcul_degres_entrant_sortant(Matrice M, Sommet S){
+    pair<int,int> result( calcul_degres_entrant(M, S.getID()) , calcul_degres_sortant(S) );
 
      return result;
-    }
+}
 
 vector<int> coloration_Graphe(Graphe G){}
 
@@ -46,18 +212,17 @@ vector<vector<int>> stables_Graphe(Matrice M){}
 vector<vector<int>> cliques_Graphe(Matrice M){}
 
 vector<int> voisin_sommet(Matrice M, int ID){
-    
-    /* déclare variable vector vide  */
+     /* déclare variable vector vide  */
     vector<int> voisins;
      
      /* ittération sur le nombre de sommet de la matrice M  */
         for (int i=0; i < M.gettV() ; i++){
-            if ( M.getTab()[i][ID] ) {
+            if ( M.getTab()[ID][i] )  {
                 voisins.insert(voisins.end(),i);
             }
         }
     return voisins;
-    }
+}
 
 int gestion_flots(Graphe G, int ID_source, int ID_puit){}
 
@@ -75,6 +240,8 @@ vector<vector<int>> chaine_eulerienne(Matrice M){}
 
 vector<vector<int>> chaine_hamiltonienne(Matrice M){}
 
-vector<int> postier_chinois(Matrice M){}
+vector<int> postier_chinois(Matrice M){
+
+}
 
 vector<int> voyageur_de_commerce(vector<int>, Matrice M){}
