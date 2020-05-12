aa9a97cf6e454e63888b1c710fdfc12cdad0867b
gnouf1 <jeuxforum26@gmail.com>
2020-05-12 12:24:32 +0200

    test constructeur QSommet --> :+1:

--- test/test_app/GUI/fntPrincipale/QSommet.cc
+++ test/test_app/GUI/fntPrincipale/QSommet.cc
@@ -5,6 +5,36 @@ class TestQSommet: public QObject
 {
     Q_OBJECT
 private slots:
+  void testConstructQSomment(){
+    // On fait la map INUTILE du sommet
+    VectVal vv;
+    vv.type = 0;
+    vv.valeur_entiere = 12;
+
+    map <string, VectVal> cu;
+    cu.insert (pair <string, VectVal> ("plus_tot", vv));
+    cu.insert (pair <string, VectVal> ("plus_tard", vv));
+
+    // On construit le Sommet
+    Sommet S0(23, 32, "sommet0", 0, cu);
+
+    // On construit le QSommet
+    QSommet QS0(S0);
+
+    QVERIFY(QS0.getID() == S0.getID());
+    QVERIFY(QS0.getPosX() == S0.getPosX());
+    QVERIFY(QS0.getPosY() == S0.getPosY());
+    QVERIFY(QS0.getRayon() == TAILLE_RAYON);
+    QVERIFY(QS0.getSelect() == 0);
+  }
+
+
+  void TestGetters();
+  void TestSetters();
+
+  void TestmouseDoubleClickEvent(QGraphicsSceneMouseEvent *event);
+  void TestboundingRect();
+  void Testpaint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 };
 
 #ifndef QTT_MAIN
