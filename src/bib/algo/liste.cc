df4ce8c5b92b479b8bbcefef6d22be0e8ef2c564
gnouf1 <jeuxforum26@gmail.com>
2020-05-20 20:47:04 +0200

    ajout connexité

--- src/bib/algo/liste.cc
+++ src/bib/algo/liste.cc
@@ -1008,7 +1008,76 @@ Graphe anti_arborescence(Graphe G){
     return A;
 }
 
-int connexite(Matrice M){}
+int connexite(Matrice M)
+{
+	if(M.getType()!=0)
+	{
+		return -1;
+	}
+	vector<vector <int>> T = M.getTab(), ListeVoisin;
+
+	vector<int> Marque, ListeSommet;	// 1 si marqué, 0 sinon ?
+
+
+
+
+	for(int y = 0; y<M.gettV(); y++)
+	{
+		Marque.push_back(0);
+		ListeVoisin.push_back({});
+		ListeSommet.push_back(y);	//initialisation à vide
+	}
+
+
+	int i,j;
+
+	for(i=0; i<M.gettV(); i++)	//boucle sommet départ
+	{
+		for(j=0; j<M.gettV(); j++)	//On teste tous les sommets de la matrice
+		{
+
+			if(T[i][j]==1)	//Si il existe un arc entre i et j
+			{
+				if(Marque[i]!=1)	//Si j n'est pas encore marqué par le programme
+				{
+					Marque[i] = 1;
+					ListeVoisin[i].push_back(j);	//On l'ajoute à la liste des successeurs
+				}
+
+			}
+		}
+	}
+
+	for(i=0; i<ListeVoisin.size(); i++)
+	{
+		for(j=0; j<ListeVoisin[i].size(); j++)
+		{
+			ListeSommet[ListeVoisin[i][j]] = -1;
+			ListeSommet[i] = -1;
+		}
+	}
+
+	int flag = 0;
+
+
+	for(i=0; i<ListeSommet.size(); i++)
+	{
+		if(ListeSommet[i]!=-1)
+		{
+
+			flag = 1;
+		}
+	}
+
+	if(flag == 0)
+	{
+		return 1; //graphe connexe
+	}
+
+	return 0; 	//graphe non connexe
+
+}
+
 
 vector<vector<int>> chaine_eulerienne(Matrice M){
   vector<int> path;
