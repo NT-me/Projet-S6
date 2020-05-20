19723caa2a42845b4cad98f9a235e86dc626a180
TRektFR <78340alex@gmail.com>
2020-05-20 09:10:00 -0700

    ajout de ordo create

--- src/app/GUI/fntOrdoCreate/OCDialog.hh
+++ src/app/GUI/fntOrdoCreate/OCDialog.hh
@@ -1,36 +1,28 @@
-// class Ui_Dialog {
-// public:
-//     QTableWidget *tableWidget;
-//     QLineEdit *DureelineEdit;
-//     QLineEdit *TacheAnterieurlineEdit;
-//     QLineEdit *IDlineEdit;
-//     QLineEdit *nomDeLaTachelineEdit;
-//     QPushButton *ajoutTacheButton;
-//     QPushButton *supprimeTacheButton;
-//     QDialogButtonBox *buttonBox;
-//     void setupUi(QDialog *Dialog)
-//     void retranslateUi(QDialog *Dialog)
-//
-// };
-//
-// namespace Ui {
-// class ordoCreate;
-// }
-//
-//
-// class ordoCreate : public QDialog {
-// Q_OBJECT
-//
-// public:
-//     ordoCreate(QWidget *parent = nullptr);
-//     ~ordoCreate();
-//
-// private:
-//     Ui::ordoCreate *ui;
-//     vector<row_pert> res;
-//
-// public slots:
-//
-//     void ajoutTache();
-//     void supprimerTache();
-// };
+#include "UI_ordoCreate.hh"
+#include <vector>
+#include "../../../bib/bib.hh"
+#include <QStringList>
+#include <QString>
+
+namespace Ui {
+class Dialog;
+}
+
+
+class ordoCreate : public QDialog {
+Q_OBJECT
+private:
+  Ui::Dialog *ui;
+  vector<pert_row> res;
+
+public:
+    ordoCreate(QWidget *parent = nullptr);
+    ~ordoCreate();
+    vector<pert_row> getRes();
+
+
+public slots:
+
+    void ajoutTache();
+    void supprimerTache();
+};
